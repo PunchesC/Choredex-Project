c5815193e1f6f05dec0114f17ecea87c0f442cb4
Yitz <yhochstadt@gmail.com>
2021-06-09 17:06:05 -0400

    setup

---
+++ functions/src/routes/homepage.ts
@@ -0,0 +1,14 @@
+import * as functions from "firebase-functions";
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message:"Hello"});
+});
+
+
+export default functions.https.onRequest(app);

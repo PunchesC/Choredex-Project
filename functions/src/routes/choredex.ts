20f892d2bb8d14fc72be4ab743aa412b0d9e34c7
kaleighgriffin <kaleigh200@gmail.com>
2021-06-10 15:40:40 -0400

    pulling pokemon api in progress

--- functions/src/routes/choredex.ts
+++ functions/src/routes/choredex.ts
@@ -1,6 +1,7 @@
 // import * as functions from "firebase-functions";
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 // import { Result } from '../model/pokemonAPI';
 
 const pokemonRoutes = express.Router();
@@ -15,8 +16,8 @@ app.get("/", (req, res) => {
 
 pokemonRoutes.get("/", async (req, res) => {
     try {
-      const results = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
-      res.json(results); // send JSON results
+      const results = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151').then(res => res.data);
+        res.json(results); // send JSON results
     } catch (err) {
       console.error("FAIL", err);
       res.status(500).json({message: "Internal Server Error"});

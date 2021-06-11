5b0faabd8454c008e4aa10f522537d29de52eda1
Yitz <yhochstadt@gmail.com>
2021-06-11 11:38:45 -0400

    got-the-stuff

--- functions/src/model/pokemonAPI.ts
+++ functions/src/model/pokemonAPI.ts
@@ -1,19 +1,29 @@
-export interface PokemonResults{
+export interface PokemonResults {
   results: Result[];
- }
- 
- export interface Result{
-   name: string;
-   url: string;
- }
- export interface PokeData{
-   stats: Stats[];
-   order:number;
- }
- export interface Stats{
-   base_stat: number;
-   stat: Stat;
- }
- export interface Stat{
-   name:string;
- }
+}
+
+export interface Result {
+  name: string;
+  url: string;
+}
+export interface PokeData {
+  stats: Stats[];
+  order: number;
+  sprites: AlmostPic;
+}
+export interface AlmostPic {
+  other: GrabPic;
+}
+export interface GrabPic {
+  ['official-artwork']: Pic;
+}
+export interface Pic {
+  front_default: string;
+}
+export interface Stats {
+  base_stat: number;
+  stat: Stat;
+}
+export interface Stat {
+  name: string;
+}
--- functions/src/routes/choredex.ts
+++ functions/src/routes/choredex.ts
@@ -3,23 +3,16 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 import {PokeData, PokemonResults} from '../model/pokemonAPI';
-// import { Result } from '../model/pokemonAPI';
+import { getClient } from "../db";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// pokemonRoutes.get("/stuff", (req, res) => {
-//     getPokeStuff();
-// });
-
-
 app.get("/", async (req, res) => {
     try {
-      await getPokeStuff()
-     
-      res.send(getPokeStuff);
-      console.log(getPokeStuff)
+      await getPokeStuff();
+      res.send()
     } catch (err) {
       console.error("FAIL", err);
       res.status(500).json({message: "Internal Server Error"});
@@ -27,13 +20,14 @@ app.get("/", async (req, res) => {
 });
 
 async function getPokeStuff(){
-  let pokeResults:PokemonResults = (await axios.get('https://pokeapi.co/api/v2/pokemon?limit=3')).data;
+  let pokeResults:PokemonResults = (await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')).data;
   let pokes = pokeResults.results
   console.log(pokes);
-  let dbArray: { name: string; hp: string; hpAmount: number; attack: string; attackAmount: number; defense: string; defenseAmount: number; speed: string; speedAmount: number; }[]= [];
+  let dbArray: { id: number; name: string; hp: string; hpAmount: number; attack: string; attackAmount: number; defense: string; defenseAmount: number; speed: string; speedAmount: number; picUrl: string;}[]= [];
   for (let poke of pokes){
     let pokeData:PokeData = (await axios.get(poke.url)).data
     let dbPoke = {
+      id: pokeData.order,
       name: poke.name,
       hp: pokeData.stats[0].stat.name,
       hpAmount: pokeData.stats[0].base_stat,
@@ -43,11 +37,14 @@ async function getPokeStuff(){
       defenseAmount: pokeData.stats[2].base_stat,
       speed: pokeData.stats[5].stat.name,
       speedAmount: pokeData.stats[5].base_stat,
+      picUrl: pokeData.sprites.other['official-artwork'].front_default
     }
     dbArray.push(dbPoke);
 
   }
-  
+  getClient().then(client => {
+  client.db().collection('Pokemon').insertMany(dbArray);
+  })
 }
 
 

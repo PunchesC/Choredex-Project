67dda2595c6219dea6eacff1bb7d3d6a2aba5d12
Yitz <yhochstadt@gmail.com>
2021-06-11 17:06:57 -0400

    math

--- frontend/src/components/Pokemons.tsx
+++ frontend/src/components/Pokemons.tsx
@@ -6,10 +6,48 @@ import { allPokemon } from "../service/pokemonService";
 function Pokemons(){
  const [pokes, setPokes] = useState<Pokemon[]>([])
 
+ let avgPoints = 0;
+ let maxPoints = 0;
+ let minPoints = 500;
+ for (let poke of pokes){
+   avgPoints += poke.hpAmount + poke.defenseAmount + poke.attackAmount + poke.speedAmount
+ }
+ console.log(avgPoints / 151)
+for (let poke of pokes){
+  let currentMax = poke.hpAmount + poke.defenseAmount + poke.attackAmount + poke.speedAmount
+  if (currentMax > maxPoints){
+    maxPoints = currentMax;
+  }
+}
+for (let poke of pokes){
+  let currentMin = poke.hpAmount + poke.defenseAmount + poke.attackAmount + poke.speedAmount
+  if (currentMin < minPoints){
+    minPoints = currentMin;
+  }
+}
+console.log(maxPoints)
+console.log(minPoints)
+let easyPokes = []
+let mediumPokes = []
+let hardPokes = []
+for (let poke of pokes){
+  let points = poke.hpAmount + poke.defenseAmount + poke.attackAmount + poke.speedAmount
+  if (points >= 150 && points < 280){
+    easyPokes.push(poke)
+  } else if (points >= 280 && points < 340){
+    mediumPokes.push(poke)
+  } else {
+    hardPokes.push(poke)
+  }
+}
+console.log(easyPokes.length)
+console.log(mediumPokes.length)
+console.log(hardPokes.length)
+
+
  useEffect(() => {
   allPokemon().then((data) => {
     setPokes(data);
-    console.log(data);
   });
 }, []);
 

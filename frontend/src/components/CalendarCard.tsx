dfb6138357b4c84e8206bd6bcf7dd192939c219a
Curtis <c.b.punches@gmail.com>
2021-06-14 09:52:04 -0400

    TaskForm componenets

--- frontend/src/components/CalendarCard.tsx
+++ frontend/src/components/CalendarCard.tsx
@@ -1,15 +1,31 @@
 import "./CalendarCard.css"
 import {useContext} from "react"
 import {ChoreContext} from "../context/chore.context"
+import TaskForm from "./TaskForm";
+import { Chore } from "../model/model";
 
 
 function CalendarCard(){
 const {chores} = useContext(ChoreContext);
 
+function handleAddTask(){
+  console.log("hi")
+}
+
+
+
+
   return(
     <div className="CalendarCard">
       <h5>[trainer name] calendar</h5>
       <button>create a task for [trainer name]</button>
+      <h5>[Trainer Name] Calendar</h5>
+      <section >
+      <button>Create a Task for [Trainer Name]</button>
+      <section className="CalendarCard_taskform">
+    <TaskForm onSubmit={handleAddTask} />
+    </section>
+    </section>
       <div className="CalendarCard_days">
         <div>monday
           {chores.map((chores)=> (

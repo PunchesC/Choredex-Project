394bcb485739807fbe7fba7c1a1e99209c95a28a
kaleighgriffin <kaleigh200@gmail.com>
2021-06-17 12:09:35 -0400

    one more fix

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -65,7 +65,7 @@ function testing(){
             {!currentUser&&<NavLink to="/sign-in-form" className={"navLinks"+ addNoDisplay} onClick={testing}>sign in</NavLink>}
             {!currentUser&&<NavLink to="/account-sign-up" className={"navLinks"+ addNoDisplay}>sign up</NavLink>}
             {currentUser&& <span className="welcomeMessage">Welcome {currentUser}, to the {account.gymName}!</span>}
-            {currentUser===account.adminName && <button className="NavLinkButton" onClick={goToAdminHomepage}>view my homepage</button>}
+            {currentUser===account.adminName && <button className="NavLinkButton" onClick={goToAdminHomepage}>view homepage</button>}
             {currentUser && <button className="NavLinkButton" onClick={goToChoredex}>view choredex</button>}
             {currentUser&&<button className="NavLinkButton" onClick={signOut}>sign out</button>}
           </nav>

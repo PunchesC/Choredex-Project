17d28845c281657910903bcfac9db91f5ddcc861
Curtis <c.b.punches@gmail.com>
2021-06-21 15:44:11 -0400

    testing

---
+++ frontend/src/components/SignInForm.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import SignInForm from './SignInForm';
+
+
+test('h2 text', ()=>{
+  render(<SignInForm />);
+  const text = screen.getByText("SIGN IN FORM");
+  expect(text).toBeInTheDocument
+});
+
+
+test('password text', ()=>{
+  render(<SignInForm />);
+  const text = screen.getByText("password:");
+  expect(text).toBeInTheDocument
+});
+
+
+
